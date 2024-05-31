3eba53e9c2a9d7b3c0907a351ab94e3100f35826
Arseniy <zubkov_000@list.ru>
2024-05-31 19:20:20 +0300

    5

--- myproject/myapp5/admin.py
+++ myproject/myapp5/admin.py
@@ -1,6 +1,92 @@
 from django.contrib import admin
 from .models import Review, OrderComment
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['product', 'user_name', 'comment', 'published_date']
+    ordering = ['-published_date']  
+    list_filter = ['product', 'user_name'] 
+    search_fields = ['comment']  
 
-# admin.site.register(Review)
-# admin.site.register(OrderComment)
+    fieldsets = [
+        (
+            'Сведения об обзоре',
+            {
+                'classes': ['wide'],
+                'fields': [
+                    ('Продукт', 'product'), 
+                    'user_name',
+                    'comment',
+                ],
+            },
+        ),
+        (
+            'Метаданные',
+            {
+                'classes': ['collapse'],
+                'fields': [
+                    'published_date', 
+                ],
+            },
+        ),
+    ]
+
+class OrderCommentAdmin(admin.ModelAdmin):
+    list_display = ['order', 'user_name', 'comment', 'comment_date']
+    ordering = ['-comment_date'] 
+    list_filter = ['order', 'user_name']  
+    search_fields = ['comment']  
+
+    fieldsets = [
+        (
+            'Сведения о комментарии',
+            {
+                'classes': ['wide'],
+                'fields': [
+                    ('Заказ', 'order'), 
+                    'user_name',
+                    'comment',
+                ],
+            },
+        ),
+        (
+            'Метаданные',
+            {
+                'classes': ['collapse'],
+                'fields': [
+                    'comment_date',
+                ],
+            },
+        ),
+    ]
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'user', 'quantity', 'added_date', 'total_price'] 
+    ordering = ['-added_date']  
+    list_filter = ['product', 'user']  
+    search_fields = ['product__name'] 
+
+    fieldsets = [
+        (
+            'Сведения о товаре в корзине',
+            {
+                'classes': ['wide'],
+                'fields': [
+                    ('Продукт', 'product'), 
+                    ('Пользователь', 'user'),
+                    'quantity',
+                    ('Общая стоимость', 'total_price'),
+                ],
+            },
+        ),
+        (
+            'Метаданные',
+            {
+                'classes': ['collapse'],
+                'fields': [
+                    'added_date',
+                ],
+            },
+        ),
+    ]
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(OrderComment)
