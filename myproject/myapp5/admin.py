081b8b02e88dad1573d3b2dc7e81d6cfa0e3ad82
Arseniy <zubkov_000@list.ru>
2024-05-31 19:08:02 +0300

    5

--- myproject/myapp5/admin.py
+++ myproject/myapp5/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Review, OrderComment
 
 
-admin.site.register(Review)
-admin.site.register(OrderComment)
+# admin.site.register(Review)
+# admin.site.register(OrderComment)
--- myproject/myapp5/models.py
+++ myproject/myapp5/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from myapp3.models import Product, Order
+from myapp3.models import Client, Product, Order
 
 
 class Review(models.Model):
@@ -22,3 +22,16 @@ class OrderComment(models.Model):
 
     def __str__(self):
         return f'Комментарий к заказу {self.order.id} by {self.user_name}'
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    user = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveIntegerField(default=1)  
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('product', 'user') 
+
+    def __str__(self):
+        return f'{self.quantity}x {self.product.name} (added by {self.user.username})'
